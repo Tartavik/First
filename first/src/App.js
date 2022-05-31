4d7cb072ae7b7d87cbd30897fd9e389736e79a25
Tartavik <81617514+Tartavik@users.noreply.github.com>
2022-05-31 13:36:19 +0300

    added eslint and added congiguretions

--- first/src/App.js
+++ first/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
+import React from 'react';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"#"} className="App-logo" alt="logo" />
+        <img src={'#'} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
